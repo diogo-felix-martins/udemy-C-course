f6cf4f7cf258ffc97b9c29997c9b03e03e99ee6f
Diogo Martins <diogo.felix.martins@gmail.com>
2020-08-20 19:36:19 +0100

    Fix format specifier;

--- pointers/basics/basics.c
+++ pointers/basics/basics.c
@@ -4,7 +4,7 @@ int main () {
     int value = 10;
     int *pvalue = &value;
 
-    printf("Variable's address: %d\n", &value); 
+    printf("Variable's address: %p\n", &value); 
     printf("Pointer's address: %d\n", (void*)&pvalue); 
     printf("Pointer's value: %p\n", pvalue);
     printf("Value the pointer is pointing to: %d\n", *pvalue);
