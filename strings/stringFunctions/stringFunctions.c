0ed24c9237a563c14fe781930808040892e90113
Diogo Martins <diogo.felix.martins@gmail.com>
2020-08-20 18:13:22 +0100

    Added stringFunctions;

---
+++ strings/stringFunctions/stringFunctions.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int strLength(char text[]){
+    int length = 0;
+    while ( text[length] != '\0')
+        length++;
+    
+    return length;
+}
+
+void strConcat(char result[], char text[], char text2[]){
+    int textSize = strLength(text);
+    int textSize2 = strLength(text2);
+
+    for ( int i = 0 ; i < textSize ; i++)
+        result[i] = text[i];
+    
+    for ( int i = 0 ; i < textSize2 ; i++)
+        result[i + textSize] = text2[i];
+}
+
+_Bool strCompare(char text[], char text2[]){
+    if (strLength(text) == strLength(text2)){
+        for ( int i = 0 ; i < strLength(text) ; i++ ){
+            if ( text[i] != text2[i] )
+                return 0;
+        }
+    }
+
+    return 1;
+}
+
+int main(){
+    char text[] = "ABCD";
+    char text2[] = "EFGH";
+    char strcatResult[10];
+
+    printf("strlen: %d\n", strLength(text));
+    strConcat(strcatResult, text, text2);
+    printf("strConcat: %s\n", strcatResult);
+    printf("strCompare: %d", strCompare(text, text2));
+
+    return 0;
+}
---
+++ strings/stringFunctions/stringFunctions.md
@@ -0,0 +1,1 @@
+This program implements standard string functions like strlen, strcat, strcmp.
